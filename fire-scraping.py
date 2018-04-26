b9d35ac98e20a4488d6bc2cacbb8c654e51006eb
julianyraiol <jrr.snf@uea.edu.br>
2018-04-26 19:51:12 -0400

    feat: script pyrebase

--- fire-scraping.py
+++ fire-scraping.py
@@ -15,3 +15,27 @@ config = {
 #Initialization#
 firebase = pyrebase.initialize_app(config)
 
+db = firebase.database()
+
+#Functions
+def createCandidate(name, group, url):
+	data = {'name': name, 'group' : group}
+	
+	key = db.child('candidate').push(data)
+
+	return key['name']
+
+def scrapingCandidate(key, url):
+	scraping = countSearch(url)
+
+	data = {'link' : url, 'analysis' : scraping}
+	db.child('candidate').child(key).child('url').push(data)	
+
+#Call
+
+name = 'Levi Fidelix'
+group = 'PSC'
+url = "http://congressoemfoco.uol.com.br/eleicoes-2014/programa-de-governo-de-levy-fidelix/"
+
+key = createCandidate(name, group, url)
+scrapingCandidate(key, url)
--- scrapingcandidates.py
+++ scrapingcandidates.py
@@ -1,11 +1,17 @@
 
 # coding: utf-8
 
+# In[1]:
+
+
 import requests
 import re
 from bs4 import BeautifulSoup
 import pandas as pd
 import numpy as np
+
+
+# In[7]:
 
 
 def countSearch(page):
@@ -27,92 +33,3 @@ def countSearch(page):
                  candidate[key]+=str(soup.find_all('p')).upper().count(w.upper())
     
     return candidate
-
-
-# ## Paulo Rabello de Castro (PSC)
-
-# In[5]:
-
-
-#trocar esse site pois não mostra as propostas do candidato
-#countSearch("https://www1.folha.uol.com.br/poder/2018/03/filiado-ao-psc-rabello-de-castro-deixa-bndes-para-disputar-eleicoes.shtml")
-countSearch("http://congressoemfoco.uol.com.br/eleicoes-2014/programa-de-governo-de-levy-fidelix/")
-
-
-# ## Michel Temer (MDB)
-# 
-
-# In[19]:
-
-
-#trocar esse site pois não mostra as propostas do candidato
-countSearch("http://micheltemer.com.br/frases/")
-
-
-# ## Henrique Meirelles (PSD)
-
-# In[20]:
-
-
-#trocar esse site pois não mostra as propostas do candidato
-countSearch("http://investimentosenoticias.com.br/eleicoes/henrique-meirelles-candidato-a-presidencia-em-2018")
-
-
-# ## Aldo Rebelo e Beto Albuquerque (PSB)
-
-# In[21]:
-
-
-#trocar esse site pois não mostra as propostas do candidato
-
-countSearch("https://exame.abril.com.br/brasil/solidariedade-lanca-aldo-rebelo-como-pre-candidato-a-presidente/")
-
-
-# ## Valéria Monteiro (PMN)
-
-# In[22]:
-
-
-
-countSearch("https://veja.abril.com.br/blog/veja-gente/valeria-monteiro-candidata-a-presidente-sou-uma-via-de-reacao/")
-
-
-# In[23]:
-
-
-#trocar esse site pois não mostra as propostas do candidato
-countSearch("https://noticias.uol.com.br/politica/eleicoes/2018/noticias/2018/03/05/pre-candidata-ex-jornalista-da-globo-estuda-usar-bordao-no-estilo-eneas.htm")
-
-
-# ## Rodrigo Maia (DEM)
-
-# In[24]:
-
-
-#trocar esse site pois não mostra as propostas do candidato
-countSearch("http://www.gazetadopovo.com.br/eleicoes/2018/dem-lanca-maia-para-presidente-e-divulga-proposta-liberal-humanista-414ifo7ajuye81q47t1ay9s41")
-
-
-# ## Marina Silva (Rede)
-
-# In[25]:
-
-
-countSearch("http://marinasilva.org.br/programa/#pagina-107")
-
-
-# ## Manuela D'Ávila (PC do B)
-
-# In[26]:
-
-
-#trocar esse site pois não mostra as propostas do candidato
-countSearch("http://bardebatom.com.br/noticia/manuela-davila-conheca-a-candidata-a-presidencia")
-
-
-# In[27]:
-
-
-#trocar esse site pois não mostra as propostas do candidato
-countSearch("http://www.redebrasilatual.com.br/politica/2017/11/pre-candidata-manuela-davila-defende-consulta-para-revogar-medidas-de-temer")
-
